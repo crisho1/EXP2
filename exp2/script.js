4de587a925f0222036db2fdb8db06e086e66a645
crisho1 <80707082+crisho1@users.noreply.github.com>
2023-05-16 10:52:39 -0400

    Add files via upload
    experiencia 2 de pagina web

---
+++ exp2/script.js
@@ -0,0 +1,12 @@
+function iniciarMap(){
+    var coord = {lat:-33.5200379 ,lng: -70.8025815};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+
